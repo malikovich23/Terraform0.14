bea25867ae8f99b489758cc14f62f88d2efd1303
EC2 Default User <ec2-user@ip-172-31-43-214.eu-west-2.compute.internal>
2022-05-19 20:05:09 +0000

    latest tf changes

---
+++ Class9/Team2/README.md
@@ -0,0 +1,2 @@
+
+[![Watch the video](https://i.imgur.com/vKb2F1B.png)](https://player.vimeo.com/video/519329274)
--- Class9/vpc.tf
+++ Class9/vpc.tf
@@ -20,22 +20,22 @@ module "vpc" {
 
 
 output "public_subnet1" {
-  value = "${module.vpc.public_subnets[0]}"
+  value = module.vpc.public_subnets[0]
 }
 output "public_subnet2" {
-  value = "${module.vpc.public_subnets[1]}"
+  value = module.vpc.public_subnets[1]
 }
 output "public_subnet3" {
-  value = "${module.vpc.public_subnets[2]}"
+  value = module.vpc.public_subnets[2]
 }
 output "private_subnet1" {
-  value = "${module.vpc.private_subnets[0]}"
+  value = module.vpc.private_subnets[0]
 }
 output "private_subnet2" {
-  value = "${module.vpc.private_subnets[1]}"
+  value = module.vpc.private_subnets[1]
 }
 output "private_subnet3" {
-  value = "${module.vpc.private_subnets[2]}"
+  value = module.vpc.private_subnets[2]
 }
 output "vpc" {
   value = module.vpc.vpc
